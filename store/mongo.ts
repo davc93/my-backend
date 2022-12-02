b212465f92b27dcfa76772cb4affeb85b768e659
Diego Vergara Casanova <davc93@gmail.com>
2022-12-02 18:08:56 -0300

    se conecta a mongo atlas

--- store/mongo.ts
+++ store/mongo.ts
@@ -1,15 +1,14 @@
 import { config } from "../config";
 import { MongoClient,ObjectId } from "mongodb";
 const DB_NAME = 'davc93'
-const atlas = false
-const uri =  atlas ? `mongodb+srv://${config.MONGO.USERNAME}:${config.MONGO.PASSWORD}@mercadopago-davc93.sjfl7ar.mongodb.net/?retryWrites=true&w=majority`: `mongodb://${config.MONGO.USERNAME}:${config.MONGO.PASSWORD}@localhost:27017/`
+const atlas = true
+const uri =  atlas ? `mongodb+srv://${config.MONGO.USERNAME}:${config.MONGO.PASSWORD}@cluster0.6vkfiwi.mongodb.net/?retryWrites=true&w=majority`: `mongodb://${config.MONGO.USERNAME}:${config.MONGO.PASSWORD}@localhost:27017/`
 const client = new MongoClient(uri);
+const database = client.db(DB_NAME);
 async function insert(data: any, collection: string) {
 
         
-    const database = client.db(DB_NAME);
     const getCollection = database.collection(collection);
-    // Query for a movie that has the title 'Back to the Future'
     const result =  await getCollection.insertOne(data)
     console.log(result.insertedId);
     return result
@@ -21,7 +20,6 @@ async function upsert(data: any, collection: string) {
     return insert(data, collection);
 }
 async function list(collection:string) {
-    const database = client.db(DB_NAME);
     const getCollection = database.collection(collection);
     const result = await getCollection.find({}).toArray()
     console.log(result);
@@ -30,7 +28,6 @@ async function list(collection:string) {
 
 }
 async function get(id:string,collection:string) {
-    const database = client.db(DB_NAME);
     const getCollection = database.collection(collection);
     const mongoId = new ObjectId(id)
     const result = await getCollection.findOne({"_id":mongoId})
